702a3d89adbaefcaa726f5cf7337c50455f1693e
OtavioBraga0 <amexassessoria1@gmail.com>
2020-01-20 13:45:36 -0300

    :wrench: Finalizada configuração do Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialects: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
